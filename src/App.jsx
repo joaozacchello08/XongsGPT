348497d447382b87a72fbc446152766bc244e27f
João Zacchello <xonguinhos@gmail.com>
2025-01-01 17:50:39 -0300

    maybe last commitment?

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.css"
 import { useState } from "react"
 import { GoogleGenerativeAI } from "@google/generative-ai"
+import xongs_bebe_fofo from "./assets/xongs_bebe_fofo_500x499.png"
 
 export default function App() {
   const [question, setQuestion] = useState("")
@@ -9,18 +10,23 @@ export default function App() {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
+    setAnswer("Carregando sua resposta...")
+
     const gen_ai = new GoogleGenerativeAI(import.meta.env.VITE_api_key)
     const model = gen_ai.getGenerativeModel({ model: "gemini-1.5-flash" })
 
-    const result = await model.generateContent(question)
+    const result = await model.generateContent(`${question} OBS: Escreva como se fosse um parágrafo normal, sem negritos etc.`, { maxTokens: 512 })
     setAnswer(result.response.text())
   }
 
   return (
     <div className="content">
+      <a href="https://www.instagram.com/xongs.og" target="_blank">
+        <img src={xongs_bebe_fofo} width={150} />
+      </a>
       <h1>XongsGPT, eaí?<br /><span id="only-gang-text">Only Gang</span></h1>
 
-      <div>
+      <div className="inp">
         <input
           type="text"
           placeholder="Pergunte algo..."
@@ -34,7 +40,7 @@ export default function App() {
 
       <textarea
         disabled
-        rows={15}
+        rows={10}
         cols={55}
         style={{ padding: 15, color: "black", fontSize: 18 }}
         value={answer ? answer : "Aqui aparecerá sua resposta..."}
